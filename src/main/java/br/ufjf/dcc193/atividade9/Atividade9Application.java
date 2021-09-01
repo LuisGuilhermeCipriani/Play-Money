fe3135d47fcd6bf9a725061ef7622877be438eef
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2021-09-01 16:41:50 -0300

    Cria repositório inicial

---
+++ src/main/java/br/ufjf/dcc193/atividade9/Atividade9Application.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.atividade9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade9Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufjf/dcc193/atividade9/Atividade9ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.atividade9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
