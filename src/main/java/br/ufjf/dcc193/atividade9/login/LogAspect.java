b70c6ca9fca17704dd1e530a42227728971b0ead
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2021-09-06 17:54:58 -0300

    Adiciona aspecto para após a execução

--- src/main/java/br/ufjf/dcc193/atividade9/login/LogAspect.java
+++ src/main/java/br/ufjf/dcc193/atividade9/login/LogAspect.java
@@ -1,6 +1,7 @@
 package br.ufjf.dcc193.atividade9.login;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,11 @@ public class LogAspect {
     @Before("execution(* br.ufjf.dcc193.atividade9.login.LoginController.getLogin(..))")
     public void startLog(JoinPoint jp){
         System.out.println("Método iniciando: " + jp.getSignature());
+    }
+
+    @After("execution(* br.ufjf.dcc193.atividade9.login.LoginController.getLogin(..))")
+    public void endLog(JoinPoint jp){
+        System.out.println("Método concluído: " + jp.getSignature());
     }
 
 }
