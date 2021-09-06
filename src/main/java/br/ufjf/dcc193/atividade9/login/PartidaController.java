fd483472fecc67775e23c5adb51127b79e351247
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2021-09-06 17:11:50 -0300

    Adiciona validação no campo da tela Partida

--- src/main/java/br/ufjf/dcc193/atividade9/login/PartidaController.java
+++ src/main/java/br/ufjf/dcc193/atividade9/login/PartidaController.java
@@ -1,19 +1,28 @@
 package br.ufjf.dcc193.atividade9.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PartidaController {
     
     @GetMapping("/partida")
-    public String getPartida(){
+    public String getPartida(@ModelAttribute PlayMoney playMoney, Model model){
         return "login/partida";
     }
 
     @PostMapping("/partida")
-    public String postPartida(){
-        return "redirect:/partida";
+    public String postPartida(@ModelAttribute @Validated PlayMoney playMoney, BindingResult bindingResult, Model model){
+        
+        if(bindingResult.hasErrors()){
+            return getPartida(playMoney, model);
+        } else {
+            return "redirect:/partida";
+        }
     }
 }
