e72f99b84fcdf6c808e39009bfed2ce90da967e5
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2021-09-06 11:27:08 -0300

    Adiciona mensagens customizadas para Play Money

--- src/main/java/br/ufjf/dcc193/atividade9/login/LoginController.java
+++ src/main/java/br/ufjf/dcc193/atividade9/login/LoginController.java
@@ -1,19 +1,27 @@
 package br.ufjf.dcc193.atividade9.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
     
     @GetMapping("/login")
-    public String getLogin(){
+    public String getLogin(@ModelAttribute PlayMoney playMoney, Model model){
         return "login/login";
     }
 
     @PostMapping("/login")
-    public String postLogin(){
-        return "redirect:/login";
+    public String postLogin(@ModelAttribute PlayMoney playMoney, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return getLogin(playMoney, model);
+        } else {
+            return "redirect:/login";
+        }
     }
 }
---
+++ src/main/java/br/ufjf/dcc193/atividade9/login/PlayMoney.java
@@ -0,0 +1,9 @@
+package br.ufjf.dcc193.atividade9.login;
+
+import lombok.Data;
+
+@Data
+public class PlayMoney {
+    private String partida;
+    private String conta;
+}
