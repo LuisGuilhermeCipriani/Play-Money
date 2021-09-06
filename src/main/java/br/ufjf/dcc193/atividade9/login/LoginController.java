52bd582bcf10899c651038784d9aa5c823188e4e
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2021-09-06 12:11:43 -0300

    Adiciona validação nos campos de Play Money

--- src/main/java/br/ufjf/dcc193/atividade9/login/LoginController.java
+++ src/main/java/br/ufjf/dcc193/atividade9/login/LoginController.java
@@ -3,6 +3,7 @@ package br.ufjf.dcc193.atividade9.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String postLogin(@ModelAttribute PlayMoney playMoney, BindingResult bindingResult, Model model){
+    public String postLogin(@ModelAttribute @Validated PlayMoney playMoney, BindingResult bindingResult, Model model){
 
         if(bindingResult.hasErrors()){
             return getLogin(playMoney, model);
--- src/main/java/br/ufjf/dcc193/atividade9/login/PlayMoney.java
+++ src/main/java/br/ufjf/dcc193/atividade9/login/PlayMoney.java
@@ -1,9 +1,18 @@
 package br.ufjf.dcc193.atividade9.login;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 
 @Data
 public class PlayMoney {
+
+    @Pattern(regexp = "^[a-fA-F0-9]+$")
+    @Min(6)
+    @Max(6)
     private String partida;
+
     private String conta;
 }
