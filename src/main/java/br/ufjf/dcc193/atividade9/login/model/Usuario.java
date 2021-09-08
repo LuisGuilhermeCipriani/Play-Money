de4603c4a19547ec9efc805a8f417d942cdaf78a
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2021-09-08 10:39:56 -0300

    Adiciona classe de modelo para usuário

---
+++ src/main/java/br/ufjf/dcc193/atividade9/login/model/Usuario.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc193.atividade9.login.model;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Usuario {
+    
+    private String partida;
+    private String conta;
+    private Date data;
+    private Date hora;
+    private Float montante;
+}
