8ca976e0bcc125076186d42ecd28cf23db2287fa
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2021-09-08 15:32:22 -0300

    Adiciona modelo inicial do banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO usuario(conta, partida, montante, role) 
+VALUES('123','Abc@32', 100,'ADMIN');
+
+
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS usuario (
+    conta VARCHAR(50) PRIMARY KEY,
+    partida VARCHAR(6),
+    data DATE,
+    hora DATE,
+    montante FLOAT,
+    role VARCHAR(150)
+);
